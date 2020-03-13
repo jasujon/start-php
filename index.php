d03dcf9814a78ebaa0c6f031de208cd807512e16
jasujon <jubayedalamsujon@gmail.com>
2020-03-14 01:29:36 +0600

    we remain main.php to index.php for show output

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "Hello World";
+?>
+
+<!-- for show output our http://localhost/start-php/main.php .... 
+but want to see when we search http://localhost/start-php  ... if we want see that we must be need to convert main.php to index.php  -->
